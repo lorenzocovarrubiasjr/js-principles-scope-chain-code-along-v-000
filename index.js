9f12b502e37fc84a0175a79cf6b6840a96dea305
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-08 03:49:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-/* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+var bestCustomer = undefined
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+ return bestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+  const LeastFavoriteCustomer = "not I"
+  LeastFavoriteCustomer = "I hope not I"
+}
+
+const favoriteCustomer = "Lorenzo"
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "Lorenzo"
+  let favoriteCustomer ="Imelda"
+}
