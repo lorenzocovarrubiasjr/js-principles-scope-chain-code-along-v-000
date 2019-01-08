79470ca19aaa866d4771b7527c53180ef51e4482
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-08 03:03:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-/* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  return customerName.uppercase() 
+}
+
+function setBestCustomer(customer) {
+  let bestCustomer = customer 
+  return bestCustomer
+}
